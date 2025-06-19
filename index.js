e8967e336902513ce5f8bc917844f9e119fb5e50
abhinav84094 <abhinav84094@gmail.com>
2025-06-19 17:33:36 +0530

    node server is added and CI pipeline is addedd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Welcome in JavaScript");
+import express from "express";
+
+const server = express();
+
+server.listen(3002, ()=>{
+    console.log(`Server is on : ${3002} PORT`);
+})
+
+server.get('/', (req, res) => {
+  res.send('Hello CI/CD World!');
+});
